ce6de8cfe910cf33515feccc0d81ff01d2dc73ec
KristiyanHr <20118@ukct-bg.com>
2025-03-14 09:01:43 +0200

    C8

--- load-testing/script-capitalize.js
+++ load-testing/script-capitalize.js
@@ -1,49 +1,25 @@
-// capitalizeLoadTest.js
-
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { sleep } from 'k6';
 
 export const options = {
-  scenarios: {
-    scenario1: {
-      executor: 'constant-vus',
-      vus: 20,
-      duration: '30s',
-    },
-    scenario2: {
-      executor: 'ramping-vus',
-      startVUs: 0,
-      stages: [
-        { duration: '30s', target: 50 },
-        { duration: '30s', target: 0 },
-      ],
-    },
-  },
+  vus: 10,
+  duration: '30s',
 };
 
 export default function () {
   const url = 'http://localhost:8080/capitalize';
   const payload = JSON.stringify({ text: 'hello world' });
-  const params = { headers: { 'Content-Type': 'application/json' } };
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
   const res = http.post(url, payload, params);
 
-  if (res.status !== 200) {
-    console.error(`Unexpected status code: ${res.status}. Response: ${res.body}`);
-    return;
-  }
-
-  let jsonResponse;
-  try {
-    jsonResponse = JSON.parse(res.body);
-  } catch (e) {
-    console.error('Failed to parse JSON response:', res.body);
-    return;
-  }
-
-  check(jsonResponse, {
-    'response is correct': (r) => r.text === 'HELLO WORLD',
-  });
+  console.log('Response status: ' + res.status);
+  console.log('Response body: ' + res.body);
 
   sleep(1);
 }
--- load-testing/script-login.js
+++ load-testing/script-login.js
@@ -1,26 +1,25 @@
-
 import http from 'k6/http';
-import { sleep } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+    // A number specifying the number of VUs to run concurrently.
+    vus: 10,
+    // A string specifying the total duration of the test run.
+    duration: '30s',
 };
 
 export default function () {
-  const url = 'http://localhost:8080/capitalize';
-  const payload = JSON.stringify({ text: 'hello world' });
+    const url = 'http://localhost:8080/login';
+    const payload = JSON.stringify({
+        email: 'aaa',
+        password: 'bbb',
+    });
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
 
-  const res = http.post(url, payload, params);
+    http.post(url, payload, params);
 
-  console.log('Response status: ' + res.status);
-  console.log('Response body: ' + res.body);
-
-  sleep(1);
 }
